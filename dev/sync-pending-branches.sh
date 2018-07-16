85c57b09d290d2e29e818264fd02041e708d0cd8
Ximin Luo <infinity0@debian.org>
2018-07-15 20:30:29 -0700

    Add dev/sync-pending-branches.sh

---
+++ dev/sync-pending-branches.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Sync remote pending branches locally, and prune merged branches.
+set -e
+
+git fetch origin --prune
+PREVBRANCH="$(git rev-parse --abbrev-ref HEAD)"
+git branch --merged | grep ^pending- | xargs -trn1 git branch -d
+git branch --list -r 'origin/pending-*' --format='%(refname:lstrip=3)' | xargs -trn1 git checkout
+git checkout "$PREVBRANCH"
