3cf49b22b9abb4383043b0be5b0d51b1cddc2318
Ximin Luo <infinity0@debian.org>
2018-07-29 00:45:24 -0700

    sync-pending-branches: make deletion of merged branches actually work

--- dev/sync-pending-branches.sh
+++ dev/sync-pending-branches.sh
@@ -4,6 +4,6 @@ set -e
 
 git fetch origin --prune
 PREVBRANCH="$(git rev-parse --abbrev-ref HEAD)"
-git branch --merged | grep ^pending- | xargs -trn1 git branch -d
+git branch --merged | tr -d ' ' | grep ^pending- | xargs -trn1 git branch -d
 git branch --list -r 'origin/pending-*' --format='%(refname:lstrip=3)' | xargs -trn1 git checkout
 git checkout "$PREVBRANCH"
